46b461db07f861f3df54069188f4f9ce7f1d617d
Tomas Krabac <tomas.krabac@ackee.cz>
2015-07-29 09:22:39 +0200

    code screen - show/hide according keyboard + review + changes according to PSD

--- rekola/src/main/java/cz/rekola/app/fragment/natural/BorrowFragment.java
+++ rekola/src/main/java/cz/rekola/app/fragment/natural/BorrowFragment.java
@@ -1,9 +1,17 @@
 package cz.rekola.app.fragment.natural;
 
+import android.app.Service;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
@@ -17,7 +25,9 @@ import cz.rekola.app.core.bus.MessageEvent;
 import cz.rekola.app.core.bus.dataFailed.LockCodeFailedEvent;
 import cz.rekola.app.fragment.base.BaseMainFragment;
 import cz.rekola.app.utils.KeyboardUtils;
+import cz.rekola.app.utils.SoftKeyboard;
 import cz.rekola.app.view.CodeView;
+import cz.rekola.app.view.EditTextPoint;
 
 /**
  * Screen to borrow bike
@@ -25,14 +35,25 @@ import cz.rekola.app.view.CodeView;
 
 public class BorrowFragment extends BaseMainFragment {
 
-
+    @InjectView(R.id.root_layout)
+    ScrollView mRootLayout;
     @InjectView(R.id.txt_bike_code)
     CodeView mTxtBikeCode;
+    @InjectView(R.id.txt_code_hint_keyboard_hidden)
+    TextView mTxtCodeHintKeyboardHidden;
+    @InjectView(R.id.txt_code_hint_keyboard_visible)
+    TextView mTxtCodeHintKeyboardVisible;
+    @InjectView(R.id.img_rekola_logo)
+    ImageView mImgRekolaLogo;
+    @InjectView(R.id.txt_borrow_info)
+    TextView mTxtBorrowInfo;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_borrow, container, false);
         ButterKnife.inject(this, view);
+        initKeyboardsCallBacks();
         return view;
     }
 
@@ -77,6 +98,36 @@ public class BorrowFragment extends BaseMainFragment {
         } else {
             getApp().getBus().post(new MessageEvent(getResources().getString(R.string.error_borrow_bike_failed)));
         }
+    }
+
+    private void initKeyboardsCallBacks() {
+        InputMethodManager im = (InputMethodManager) getActivity().getSystemService(Service
+                .INPUT_METHOD_SERVICE);
+
+        SoftKeyboard softKeyboard = new SoftKeyboard(mRootLayout, im);
+        softKeyboard.setSoftKeyboardCallback(new SoftKeyboard.SoftKeyboardChanged() {
+
+            @Override
+            public void onSoftKeyboardHide() {
+                mImgRekolaLogo.setVisibility(View.VISIBLE);
+                mTxtBorrowInfo.setVisibility(View.VISIBLE);
+                mTxtCodeHintKeyboardHidden.setVisibility(View.VISIBLE);
+                mTxtBikeCode.setHintVisibility(true);
+
+                mTxtCodeHintKeyboardVisible.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onSoftKeyboardShow() {
+                mImgRekolaLogo.setVisibility(View.GONE);
+                mTxtBorrowInfo.setVisibility(View.GONE);
+                mTxtCodeHintKeyboardHidden.setVisibility(View.GONE);
+                mTxtBikeCode.setHintVisibility(false);
+
+                mTxtCodeHintKeyboardVisible.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 }
